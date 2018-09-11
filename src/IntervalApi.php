e887c3c388dd5c8161cf517923d805fb0fb37d58
n1mda <axel.moller@budbee.com>
2018-09-11 10:35:13 +0200

    Add support for validating postalcodes and requesting intervals per country

--- src/IntervalApi.php
+++ src/IntervalApi.php
@@ -30,9 +30,47 @@ class IntervalApi
 
     /**
      * Get intervals
+     * @param string $country Country code
      * @param string $postalCode The postalcode to get intervals for
      * @param int $n The number of intervals you want to get
      * @return array[\Budbee\Model\OrderIntervalResponse]
+     */
+    public function getIntervals($country, $postalCode, $n)
+    {
+        //parse inputs
+        $resourcePath = "/intervals/{country}/{postalCode}/{n}";
+        $method = Client::$GET;
+        $queryParams = array();
+        $headerParams = array(
+            'Accept' => 'application/vnd.budbee.intervals-v2+json',
+            'Content-Type' => 'application/vnd.budbee.intervals-v2+json'
+        );
+
+        if (null != $n) {
+            $resourcePath = str_replace("{country}", $this->apiClient->toPathValue($country), $resourcePath);
+            $resourcePath = str_replace("{postalCode}", $this->apiClient->toPathValue($postalCode), $resourcePath);
+            $resourcePath = str_replace("{n}", $this->apiClient->toPathValue($n), $resourcePath);
+        }
+        //make the API Call
+        if (!isset($body)) {
+            $body = null;
+        }
+        $response = $this->apiClient->callAPI($resourcePath, $method, $queryParams, $body, $headerParams);
+
+        if (!$response) {
+            return null;
+        }
+
+        $responseObject = $this->apiClient->deserialize($response, 'array[\Budbee\Model\OrderIntervalResponse]');
+        return $responseObject;
+    }
+
+    /**
+     * Get intervals
+     * @param string $postalCode The postalcode to get intervals for
+     * @param int $n The number of intervals you want to get
+     * @return array[\Budbee\Model\OrderIntervalResponse]
+     * @deprecated deprecated in favour of getIntervals($country, $postalCode, $n)
      */
     public function getIntervals($postalCode, $n)
     {
@@ -65,9 +103,47 @@ class IntervalApi
 
     /**
      * Get intervals
+     * @param string $country Country code
      * @param string $postalCode The postalcode to get intervals for
      * @param string $date Get all intervals up to and including this date in format "YYYY-MM-DD".
      * @return array[\Budbee\Model\OrderInterval]
+     */
+    public function getIntervalsToDate($country, $postalCode, $date)
+    {
+        //parse inputs
+        $resourcePath = "/intervals/{country}/{postalCode}/{date}";
+        $method = Client::$GET;
+        $queryParams = array();
+        $headerParams = array(
+            'Accept' => 'application/vnd.budbee.intervals-v2+json',
+            'Content-Type' => 'application/vnd.budbee.intervals-v2+json'
+        );
+
+        if (null != $date) {
+            $resourcePath = str_replace("{country}", $this->apiClient->toPathValue($country), $resourcePath);
+            $resourcePath = str_replace("{postalCode}", $this->apiClient->toPathValue($postalCode), $resourcePath);
+            $resourcePath = str_replace("{date}", $this->apiClient->toPathValue($date), $resourcePath);
+        }
+        //make the API Call
+        if (!isset($body)) {
+            $body = null;
+        }
+        $response = $this->apiClient->callAPI($resourcePath, $method, $queryParams, $body, $headerParams);
+
+        if (!$response) {
+            return null;
+        }
+
+        $responseObject = $this->apiClient->deserialize($response, 'array[\Budbee\Model\OrderIntervalResponse]');
+        return $responseObject;
+    }
+
+    /**
+     * Get intervals
+     * @param string $postalCode The postalcode to get intervals for
+     * @param string $date Get all intervals up to and including this date in format "YYYY-MM-DD".
+     * @return array[\Budbee\Model\OrderInterval]
+     * @deprecated deprecated in favour of getIntervalsToDate($country, $postalCode, $date)
      */
     public function getIntervalsToDate($postalCode, $date)
     {
